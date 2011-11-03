e640e9c93a9f8671c97489db80b7f80575365274
Bruce Blank <Torsten.Blank@gmail.com>
2011-11-03 19:00:44 +0000

    main() restructured

--- src/ForCppTesting.cpp
+++ src/ForCppTesting.cpp
@@ -13,11 +13,11 @@
 
 using namespace std;
 
-void output(const std::string &s) {
+void output_vector_oldstyle(const std::string &s) {
   cout << " " << s;
 }
 
-int main() {
+void define_and_print_a_vector() {
 	// old style c++ to define and print a vector
 	cout << "old style: ";
 	std::vector<std::string> v1;
@@ -27,7 +27,7 @@ int main() {
 	v1.push_back("Vier");
 	v1.push_back("Fuenf");
 
-	for_each(v1.begin(), v1.end(), output);
+	for_each(v1.begin(), v1.end(), output_vector_oldstyle);
 	cout << endl;
 
 	// c++11 style to define a vector
@@ -49,6 +49,9 @@ int main() {
 	// another c++11 style to print a vector
 	cout << "new style 3: ";
 	for_each(v2.begin(), v2.end(), [](const std::string s){cout << " " << s;});
+}
 
+int main() {
+	define_and_print_a_vector();
 	return 0;
 }
