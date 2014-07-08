74b4440261475ffcfc3e07dc9a39dea8f94c7d6e
Bruce Blank <Torsten.Blank@gmail.com>
2014-07-08 17:03:43 +0200

    Added second main program and Build/Run-configuration

---
+++ src/ForCppTesting2.cpp
@@ -0,0 +1,20 @@
+/*
+ * ForCppTesting2.cpp
+ *
+ *  Created on: 08.07.2014
+ *      Author: bruce
+ */
+
+// This is a second main program, used in a second Build- and Run-Configuration.
+// Within Eclipse, choose Build-configuration in "Project -> Build Configuration" and
+// Run-Configuration with right-click on Project and then "Run As ..."
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello Bruce!" << std::endl;
+
+	return 0;
+}
+
+
